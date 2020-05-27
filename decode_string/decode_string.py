9800bd165e5cfd3b9e1722fde44046bf0755d01f
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-05-27 16:20:35 +0500

    string decoding

--- decode_string/decode_string.py
+++ decode_string/decode_string.py
@@ -1,3 +1,25 @@
 class Solution:
-    def decodeString(self, s: str) -> str:
-        pass
+    def decodeString(self, s):
+        # problem is similar to the "Balanced brackets" problem 
+        # and thus can be solved the same way with some modifications
+        stack = []
+        for i in range(len(s)):
+            # we are traversing the string looking for a closing bracket
+            # once we've found it, we go back until we find an opening bracket
+            # everythong between is a string to be multiplied by the number preceding the bracket
+            if s[i] == "]":
+                current = ''
+                while stack:
+                    val = stack.pop()
+                    if val ==  "[":
+                        break
+                    current = val + current
+                num = ''
+                while stack and stack[-1].isdigit():
+                    # this is getting the num after the opening bracket as a string
+                    # since the num could be longer than a singl digit
+                    num = stack.pop() + num
+                stack.append(int(num)*current)
+            else:
+                stack.append(s[i])
+        return ''.join(stack)
